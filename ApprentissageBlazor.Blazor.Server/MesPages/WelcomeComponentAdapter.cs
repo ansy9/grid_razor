a8724420ec5486742ee5d740bf8a96409380fc52
yanis <alla.yanis09@gmail.com>
2023-06-08 09:45:53 +0100

    creation d'un lookup avec razor

---
+++ ApprentissageBlazor.Blazor.Server/MesPages/WelcomeComponentAdapter.cs
@@ -0,0 +1,55 @@
+﻿using ApprentissageBlazor.Module.BusinessObjects;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Blazor.Editors.Adapters;
+using DevExpress.ExpressApp.Editors;
+using Microsoft.AspNetCore.Components;
+
+namespace ApprentissageBlazor.Blazor.Server.MesPages
+{
+    public class WelcomeComponentAdapter : IComponentAdapter, IComplexControl
+    {
+        private XafApplication application;
+        private RenderFragment component;
+        public RenderFragment ComponentContent
+        {
+            get
+            {
+                if (component == null)
+                {
+                    
+
+                    IObjectSpace newObjectSpace = this.application.CreateObjectSpace(typeof(DomainObject1));
+                    IList<DomainObject1> objectToShow = newObjectSpace.GetObjects<DomainObject1>( );
+                    IEnumerable<DomainObject1> domainObject1s = objectToShow.AsEnumerable();
+
+                    //objectToShow.PersistentProperty = "Alla";
+                    newObjectSpace.CommitChanges();
+
+                    component = builder => {
+                        builder.OpenComponent<WelcomeComponent>(0);
+                        builder.AddAttribute(1, nameof(WelcomeComponent.DomaineObjs), domainObject1s);
+                        builder.CloseComponent();
+                    };
+                }
+                return component;
+            }
+        }
+
+
+
+
+
+
+        public void Refresh() { }
+        public void Setup(IObjectSpace objectSpace, XafApplication application)
+        {
+            this.application = application;
+        }
+        public object GetValue()
+        {
+            return null;
+        }
+        public void SetValue(object value) { }
+        public event EventHandler ValueChanged;
+    }
+}
---
+++ ApprentissageBlazor.Module/BusinessObjects/DomainObject1.cs
@@ -0,0 +1,36 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace ApprentissageBlazor.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    
+    public class DomainObject1 : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public DomainObject1(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+        private string _PersistentProperty;
+        public string PersistentProperty
+        {
+            get { return _PersistentProperty; }
+            set { SetPropertyValue(nameof(PersistentProperty), ref _PersistentProperty, value); }
+        }
+
+        private string _PersistentProperty2;
+        public string PersistentProperty2
+        {
+            get { return _PersistentProperty2; }
+            set { SetPropertyValue(nameof(PersistentProperty2), ref _PersistentProperty2, value); }
+        }
+    }
+}
